65746853fda2f96fa1dc01d50f2e6e46c470825b
Leon The Ace Robinson <leon.robinson@qa.com>
2021-07-07 15:23:16 +0100

    added a new file

---
+++ ifstatement.py
@@ -0,0 +1,12 @@
+var1 = input("type your name:")
+if var1 == "ted":
+    print("ted")
+    print("you are logged in")
+elif var1 == "josh":
+    print("josh")
+    print("blah")
+elif var1 == "leon":
+    print("leon")
+    print("Get to work, you're late")
+else:
+    print("who are you?")
